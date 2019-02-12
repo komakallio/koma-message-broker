813568be8d8ef12af476f38ab438b070de080b98
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-02-12 22:06:27 +0200

    Emit messages without rooms

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const zmq = require('zeromq');
 const io = require('socket.io');
 
-const pubSocket = io.listen(3001, { path: '/' });
+const clients = io.listen(3001, { path: '/' });
 
 /* Wait for connection from Komakallio ZeroMQ relay */
 const subSocket = zmq.socket('sub');
 subSocket.bindSync('tcp://*:3000');
 subSocket.subscribe('');
 
-/* Publish all ZeroMQ messages with topic to corresponding Socket.io room */
+/* Forward messages coming from ZeroMQ to Socket.IO */
 subSocket.on('message', (topic, message) => {
-  pubSocket.to(topic).emit('msg', message);
+  clients.sockets.emit('msg', topic.toString(), message.toString());
 });
