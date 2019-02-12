02b4364d71af3f02a4662abdefb7e3ddadc0eb05
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2019-02-12 21:33:46 +0200

    Add custom path to Socket.IO

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const zmq = require('zeromq');
 const io = require('socket.io');
 
-const pubSocket = io.listen(3001);
+const pubSocket = io.listen(3001, { path: '/' });
 
 /* Wait for connection from Komakallio ZeroMQ relay */
 const subSocket = zmq.socket('sub');
